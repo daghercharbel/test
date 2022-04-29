4cc754c36c46fb1a34f9459cd53ef3b7f2824d13
SwapnilTT <swapnil.sen@telostouch.com>
2022-04-29 21:42:48 +0530

    APP-700: Send As Functionality

--- aura/OpenAtAndCompleteAtGraphCmp/OpenAtAndCompleteAtGraphCmpController.js
+++ aura/OpenAtAndCompleteAtGraphCmp/OpenAtAndCompleteAtGraphCmpController.js
@@ -7,7 +7,7 @@
         "-1",
         $A.getCallback((eventReceived) => {
           // Process event (this is called each time we receive an event)
-          console.log('Graph Event:: '+JSON.stringify(eventReceived));
+          //console.log('Graph Event:: '+JSON.stringify(eventReceived));
           if (
             c.get("v.recordId") ==
             eventReceived.data.payload.TelosTouchSF__Campaign__c
@@ -19,14 +19,14 @@
       .then((subscription) => {
         // Subscription response received.
         // We haven't received an event yet.
-        console.log('Subscription request sent to: ', subscription.channel);
+        //console.log('Subscription request sent to: ', subscription.channel);
         // Save subscription to unsubscribe later
       });
     h.doinit_Helper(c, e, h);
   },
   doRefresh: function (c, e, h) {
-      if (e.getParam("doRefresh") === true) {
-          h.doinit_Helper(c, e, h);
-      }
+    if (e.getParam("doRefresh") === true) {
+      h.doinit_Helper(c, e, h);
+    }
   }
 });
