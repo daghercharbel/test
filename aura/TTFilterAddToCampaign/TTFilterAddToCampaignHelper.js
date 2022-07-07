fde5f7b9c3e29c1215ad19d5dbf43fc181b6e03c
SwapnilTT <swapnil.sen@telostouch.com>
2022-07-07 19:21:51 +0530

    LOB Fix

--- aura/TTFilterAddToCampaign/TTFilterAddToCampaignHelper.js
+++ aura/TTFilterAddToCampaign/TTFilterAddToCampaignHelper.js
@@ -1,4 +1,32 @@
 ({
+    doInit_Helper : function(c, e, h){
+        c.set('v.isShowSpinner', true);
+        c.set("v.hideOptions", true);
+        var action = c.get('c.fetchLimitedRecords');
+        action.setParams({
+            campaignRecordId : c.get('v.recordId')
+        });
+        action.setCallback(this,function(response){ 
+            try{
+                //c.set('v.isShowSpinner', false);
+                var result = response.getReturnValue();
+                if(response.getState() === 'SUCCESS') {
+                    if(!$A.util.isEmpty(result)) {
+                        c.set('v.recordsList', JSON.parse(result));        
+                    } else {
+                        c.set('v.boolForNoRecord', true);
+                        
+                    }
+                }else{
+                    c.set('v.boolForNoRecord', true);
+                }
+                c.set('v.isShowSpinner', false);
+            }catch(e){
+                //console.log(e);
+            }
+        });
+        $A.enqueueAction(action);  
+    },
     selectedUserList_Helper: function (c, e, h) {
         var userList = c.get("v.userList");
         for (var i = 0; i < userList.length; i++) {
@@ -40,4 +68,38 @@
         });
         toastEvent.fire();
     },
+    
+    searchCampaign: function(c,e,h){
+        //c.set('v.isShowSpinner', true);
+        if(!$A.util.isEmpty(c.get("v.searchString"))){
+            var action = c.get('c.fetchRecords');
+            action.setParams({
+                objectName : 'Campaign',
+                filterField : 'Name',
+                searchString : c.get('v.searchString'),
+                campaignRecordId : c.get('v.recordId')
+            });
+            action.setCallback(this,function(response){ 
+                try{
+                    c.set('v.isShowSpinner', false);
+                    var result = response.getReturnValue();
+                    if(response.getState() === 'SUCCESS') {
+                        if(!$A.util.isEmpty(result) && JSON.parse(result) != '') {
+                            c.set('v.recordsList', JSON.parse(result)); 
+                            c.set("v.hideOptions", true);
+                            c.set("v.boolForNoRecord", false);
+
+                        } else {
+                            c.set('v.hideOptions', false);
+                            c.set('v.recordsList', []); 
+                            c.set("v.boolForNoRecord", true);
+                        }
+                    }
+                }catch(e){
+                    //console.log(e);
+                }
+            });
+            $A.enqueueAction(action);
+        }
+    }
 })
